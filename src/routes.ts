dcbc7cac55ae56b2ddbb5b373ea3fb2a7274e2a6
Leonardo_Soares <leonardoc.soares08@gmail.com>
2023-01-26 19:35:52 -0300

    backend pronto

--- src/routes.ts
+++ src/routes.ts
@@ -72,7 +72,7 @@ export async function appRoutes(app: FastifyInstance) {
 
     const completedHabits = day?.dayHabits.map(dayHabit => {
       return dayHabit.habit_id
-    })
+    }) ?? []
 
     return { possibleHabits, completedHabits }
 
